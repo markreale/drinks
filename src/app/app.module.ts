8538b53355c006c10283fd09cd9df916812047ed
Mark Reale <reale.mark@gmail.com>
2019-06-23 11:52:34 -0400

    Initial Firebase with Auth
    Things seem to work
    - includes services and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment.prod';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AuthService } from './auth.service';
+import { AuthGuard } from "./auth.guard";
 
 @NgModule({
   declarations: [
@@ -13,10 +23,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+      AngularFirestore,
+    AuthService,
+    AuthGuard
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
